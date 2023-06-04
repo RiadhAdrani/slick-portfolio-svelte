1bfee7e1d866f25ed80dee05ea620d87eb9e0220
RiadhAdrani <riadh_adrani@hotmail.fr>
2023-06-04 19:58:05 +0100

    chore: rebase from master

---
+++ src/lib/utils/Assets.ts
@@ -0,0 +1,143 @@
+import JavaScript from '$lib/assets/logos/js.png';
+import TypeScript from '$lib/assets/logos/ts.png';
+import Dart from '$lib/assets/logos/dart.png';
+import Kotlin from '$lib/assets/logos/kotlin.png';
+import Java from '$lib/assets/logos/java.png';
+import Python from '$lib/assets/logos/python.png';
+
+import NodeJs from '$lib/assets/logos/node.png';
+import Deno from '$lib/assets/logos/deno.png';
+
+import VueJs from '$lib/assets/logos/vue.png';
+import ReactJs from '$lib/assets/logos/react.svg';
+import Svelte from '$lib/assets/logos/svelte.png';
+import Angular from '$lib/assets/logos/angular.png';
+import Flutter from '$lib/assets/logos/flutter.svg';
+import Electron from '$lib/assets/logos/electron.png';
+import SolidJs from '$lib/assets/logos/solid.svg';
+import Quasar from '$lib/assets/logos/quasar.svg';
+import Nuxt from '$lib/assets/logos/nuxt.png';
+import Android from '$lib/assets/logos/android.png';
+
+import ExpressJs from '$lib/assets/logos/express.png';
+import Fastify from '$lib/assets/logos/fastify.svg';
+import NestJs from '$lib/assets/logos/nest.svg';
+import AdonisJs from '$lib/assets/logos/adonis.png';
+
+import MongoDB from '$lib/assets/logos/mongodb.svg';
+import Redis from '$lib/assets/logos/redis.svg';
+import PostgreSQL from '$lib/assets/logos/postgres.png';
+import Firebase from '$lib/assets/logos/firebase.png';
+
+import HTML from '$lib/assets/logos/html.svg';
+
+import CSS from '$lib/assets/logos/css.svg';
+import Sass from '$lib/assets/logos/sass.png';
+import Tailwind from '$lib/assets/logos/tailwind.svg';
+
+import Photoshop from '$lib/assets/logos/photoshop.svg';
+import AfterEffects from '$lib/assets/logos/after-effects.svg';
+import Premiere from '$lib/assets/logos/premiere.svg';
+import Illustrator from '$lib/assets/logos/illustrator.svg';
+
+import Unknown from '$lib/assets/logos/no-img.svg';
+import Csharp from '$lib/assets/logos/csharp.svg';
+import Xamarin from '$lib/assets/logos/xamarin.svg';
+
+import Docker from '$lib/assets/logos/docker.svg';
+import Kubernetes from '$lib/assets/logos/kubernetes.svg';
+import AWS from '$lib/assets/logos/aws.svg';
+import Bootstrap from '$lib/assets/logos/bootstrap.svg';
+import C from '$lib/assets/logos/c.svg';
+import Cpp from '$lib/assets/logos/cpp.svg';
+import Celery from '$lib/assets/logos/celery.svg';
+import Django from '$lib/assets/logos/django.svg';
+import FastApi from '$lib/assets/logos/fastapi.svg';
+import Flask from '$lib/assets/logos/flask.svg';
+import Go from '$lib/assets/logos/go.svg';
+import Kafka from '$lib/assets/logos/kafka.svg';
+import Neo4j from '$lib/assets/logos/neo4j.svg';
+import Nginx from '$lib/assets/logos/nginx.svg';
+import Numpy from '$lib/assets/logos/numpy.svg';
+import Pandas from '$lib/assets/logos/pandas.svg';
+import RabbitMQ from '$lib/assets/logos/rabbitmq.svg';
+import Rust from '$lib/assets/logos/rust.svg';
+import Scrapy from '$lib/assets/logos/scrapy.png';
+import Selenium from '$lib/assets/logos/selenium.svg';
+
+import CardBaker from '$lib/assets/logos/card-baker.png';
+import Gradio from '$lib/assets/logos/gradio.png';
+import RandomNamePicker from '$lib/assets/logos/gradio.png';
+import Trello from '$lib/assets/logos/trello.png';
+import BrainAcademy from '$lib/assets/logos/brain-academy.jpg';
+import HsTech from '$lib/assets/logos/hstech.jpg';
+import Recursive from '$lib/assets/logos/recursive.png';
+import TriWeb from '$lib/assets/logos/triweb.jpg';
+
+export default {
+	CardBaker,
+	Gradio,
+	RandomNamePicker,
+	Trello,
+	BrainAcademy,
+	HsTech,
+	Recursive,
+	TriWeb,
+	AWS,
+	Bootstrap,
+	C,
+	Cpp,
+	Celery,
+	Django,
+	FastApi,
+	Flask,
+	Go,
+	Kafka,
+	Neo4j,
+	Nginx,
+	Numpy,
+	Pandas,
+	RabbitMQ,
+	Rust,
+	Scrapy,
+	Selenium,
+	Docker,
+	Kubernetes,
+	Csharp,
+	Xamarin,
+	TypeScript,
+	VueJs,
+	ReactJs,
+	Dart,
+	Kotlin,
+	Python,
+	NodeJs,
+	Deno,
+	Svelte,
+	ExpressJs,
+	JavaScript,
+	Fastify,
+	NestJs,
+	Quasar,
+	SolidJs,
+	Electron,
+	Flutter,
+	Java,
+	AdonisJs,
+	Android,
+	Angular,
+	PostgreSQL,
+	Firebase,
+	Sass,
+	Unknown,
+	MongoDB,
+	Redis,
+	Tailwind,
+	HTML,
+	Premiere,
+	Photoshop,
+	CSS,
+	AfterEffects,
+	Illustrator,
+	Nuxt
+};
