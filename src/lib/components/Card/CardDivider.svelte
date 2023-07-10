5437b4d800191397786f5efd2db7923bb8e9f918
RiadhAdrani <riadh_adrani@hotmail.fr>
2023-07-10 17:56:36 +0100

    chore: split assets into a different branch

--- src/lib/components/Card/CardDivider.svelte
+++
@@ -1,9 +0,0 @@
-<div class="card-divider" />
-
-<style lang="scss">
-	.card-divider {
-		background-color: var(--border);
-		height: 1px;
-		margin: 10px 0px;
-	}
-</style>
--- src/lib/components/Icon/Icons.ts
+++
@@ -1,9 +0,0 @@
-import { Icons } from '$lib/utils';
-
-export const viewBox = (icon: Icons) => {
-	if (icon === Icons.Youtube) return '0 0 461.001 461.001';
-
-	if (icon === Icons.Facebook) return '0 0 408.788 408.788';
-
-	return [Icons.GitHub, Icons.Search, Icons.Code].includes(icon) ? '0 0 16 16' : '0 0 24 24';
-};
