1e35ef051ce122b2771c0ad5ecba30c37cd257cb
RiadhAdrani <54232641+RiadhAdrani@users.noreply.github.com>
2023-09-28 14:28:45 +0000

    Deploying to gh-pages from @ RiadhAdrani/slick-portfolio-svelte@3cb38bba239d47bc42aa06a7d3e3cce6f7e485ab 🚀

---
+++ _app/immutable/chunks/paths.715f4b20.js
@@ -0,0 +1,1 @@
+var s;const l=((s=globalThis.__sveltekit_1l1n35i)==null?void 0:s.base)??"/slick-portfolio-svelte";var e;const t=((e=globalThis.__sveltekit_1l1n35i)==null?void 0:e.assets)??l;export{t as a,l as b};
