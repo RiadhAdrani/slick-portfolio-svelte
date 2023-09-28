3cb38bba239d47bc42aa06a7d3e3cce6f7e485ab
RiadhAdrani <riadh_adrani@hotmail.fr>
2023-09-28 15:27:52 +0100

    chore: add other empty placeholders

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -72,21 +72,35 @@
 			</Banner>
 			<div class="pt-3 pb-1 overflow-x-hidden w-full">
 				<div class="px-10px m-y-5">
-					<Markdown content={data.project.description ?? 'This place is yet to be filled...'} />
+					{#if data.project.description}
+						<Markdown content={data.project.description} />
+					{:else}
+						<div class="p-5 col-center gap-3 m-y-auto text-[var(--border)]">
+							<UIcon icon="i-carbon-text-font" classes="text-3.5em" />
+							<p class="font-300">No description</p>
+						</div>
+					{/if}
 				</div>
 				<div class="w-100% m-t-8">
 					<CardDivider />
 				</div>
-				<div
-					class="px-10px grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 m-t-10 "
-				>
-					{#each screenshots as item}
-						<div class="col-center gap-3 overflow-hidden w-100% h-100% rounded-10px">
-							<img class="aspect-video w-100%" src={item.src} alt={item.label} />
-							<p class="text-[var(--tertiary-text)] font-300">{item.label}</p>
-						</div>
-					{/each}
-				</div>
+				{#if screenshots.length > 0}
+					<div
+						class="px-10px grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 m-t-10 "
+					>
+						{#each screenshots as item}
+							<div class="col-center gap-3 overflow-hidden w-100% h-100% rounded-10px">
+								<img class="aspect-video w-100%" src={item.src} alt={item.label} />
+								<p class="text-[var(--tertiary-text)] font-300">{item.label}</p>
+							</div>
+						{/each}
+					</div>
+				{:else}
+					<div class="p-5 col-center gap-3 m-y-auto text-[var(--border)]">
+						<UIcon icon="i-carbon-image" classes="text-3.5em" />
+						<p class="font-300">No screenshots</p>
+					</div>
+				{/if}
 			</div>
 		</div>
 	{/if}
