9c7af4f311a5d2de12fa0b22c63758d57b6f6221
M2K3K5 <63744077+M2K3K5@users.noreply.github.com>
2024-01-24 14:16:50 +0100

    automatic focusing of the search bar when visiting the /search page

--- src/lib/components/Input/Input.svelte
+++ src/lib/components/Input/Input.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
 	export let value = '';
 	export let placeholder = '';
+    let inputElement: HTMLInputElement;
+
+    function focus() {
+        inputElement.focus();
+    }
+
+    export { focus };
 </script>
 
 <input
+	bind:this={inputElement}
 	bind:value
 	{placeholder}
 	class="text-[inherit] bg-transparent border-[1px] border-solid border-[var(--border)] px-[20px] py-[10px] rounded-[15px] flex-1 text-[1.15em] "
