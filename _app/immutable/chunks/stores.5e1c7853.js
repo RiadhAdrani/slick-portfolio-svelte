008a146f25c2da7ec1b584fae1c5676cd7f8427e
RiadhAdrani <54232641+RiadhAdrani@users.noreply.github.com>
2024-02-22 19:07:01 +0000

    Deploying to gh-pages from @ RiadhAdrani/slick-portfolio-svelte@221a958da30adce85881df4dfe0924dcc9851e7a 🚀

--- _app/immutable/chunks/stores.5e1c7853.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./singletons.103f0cb9.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
