3351f92b332d8d908efade0ae474bdbbeeb2caaf
RiadhAdrani <riadh_adrani@hotmail.fr>
2024-03-02 19:22:34 +0100

    chore: imporve experience card

--- src/lib/components/ExperienceCard/ExperienceCard.svelte
+++ src/lib/components/ExperienceCard/ExperienceCard.svelte
@@ -8,6 +8,7 @@
 	import { getAssetURL } from '$lib/data/assets';
 	import { base } from '$app/paths';
 	import UIcon from '../Icon/UIcon.svelte';
+	import Chip from '../Chip/Chip.svelte';
 
 	export let experience: Experience;
 
@@ -21,6 +22,12 @@
 		: 'Present';
 
 	const period = `${from} - ${to} · ${months}`;
+
+	$: info = [
+		{ label: experience.company, icon: 'i-carbon-building' },
+		{ label: experience.location, icon: 'i-carbon-location' },
+		{ label: experience.contract, icon: 'i-carbon-hourglass' }
+	] as const;
 </script>
 
 <Card
@@ -30,25 +37,24 @@
 	color={experience.color}
 >
 	<div class="col md:flex-row items-start gap-5 md:gap-1">
-		<CardLogo src={getAssetURL(experience.logo)} alt={experience.company} size={75} />
+		<CardLogo src={getAssetURL(experience.logo)} alt={experience.company} size={55} />
 		<div class="col ml-0 md:ml-[20px] gap-3 w-full">
-			<h3
-				class="flex text-[0.9em] flex-col items-start sm:flex-row sm:items-center justify-between sm:gap-5 md:flex-col md:items-start md:gap-0 lg:flex-row lg:items-center"
-			>
-				<CardTitle title={experience.name} />
-			</h3>
-			<div class="row flex-wrap m-b-2 gap-1 text-0.9em font-300">
-				<ChipIcon name={`Company: ${experience.company}`}>
-					<UIcon icon="i-carbon-building" />
-				</ChipIcon>
-				<ChipIcon name={`Location: ${experience.location}`}>
-					<UIcon icon="i-carbon-location" />
-				</ChipIcon>
-				<ChipIcon name={`Contract: ${experience.contract}`}>
-					<UIcon icon="i-carbon-hourglass" />
-				</ChipIcon>
+			<div class="col ">
+				<h3
+					class="flex text-[0.9em] flex-col items-start sm:flex-row sm:items-center justify-between sm:gap-5 md:flex-col md:items-start md:gap-0 lg:flex-row lg:items-center"
+				>
+					<CardTitle title={`${experience.name}`} />
+				</h3>
+				<div class="row flex-wrap items-start m-b-2 gap-1 text-0.9em font-300">
+					{#each info as item}
+						<Chip>
+							<UIcon icon={item.icon} />
+							<span class="m-l-1">{item.label}</span>
+						</Chip>
+					{/each}
+				</div>
 			</div>
-			<div class="text-[var(--accent-text)] text-[0.9em] font-200">{period}</div>
+			<div class="text-[var(--text)] text-[0.9em] font-200">{period}</div>
 			<div class="experience-description">{experience.shortDescription}</div>
 			<div class="flex flex-row flex-wrap mt-5">
 				{#each experience.skills as skill}
