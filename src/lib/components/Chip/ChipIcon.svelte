4716147d7206164990ce4b3383af46b33d89ad67
RiadhAdrani <riadh_adrani@hotmail.fr>
2024-03-02 19:07:04 +0100

    fix(ChipIcon): tooltip hidden by other chips

--- src/lib/components/Chip/ChipIcon.svelte
+++ src/lib/components/Chip/ChipIcon.svelte
@@ -12,7 +12,7 @@
 <svelte:element
 	this={href ? 'a' : 'div'}
 	{href}
-	class={`chip-icon row-center relative text-inherit decoration-none p-10px m-r-5px m-b-5px border-1px border-solid border-[var(--border)] hover:border-[var(--border-hover)] rounded-10px ${
+	class={`chip-icon row-center relative text-inherit decoration-none p-10px m-r-5px m-b-5px border-1px border-solid border-[var(--border)] hover:border-[var(--border-hover)] rounded-10px hover:z-5 ${
 		href ? 'cursor-pointer' : 'cursor-help'
 	} ${grayscale ? 'grayscale-65 hover:grayscale-0' : ''}`}
 	data-help={name}
