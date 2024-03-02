d11437caa3ea1fafa1a475f3b21467eaa4cd6315
RiadhAdrani <riadh_adrani@hotmail.fr>
2024-03-02 21:50:37 +0100

    chore: adapt to new changes

--- src/lib/data/home.ts
+++ src/lib/data/home.ts
@@ -1,41 +1,31 @@
 import { Platform } from '$lib/types';
-import { getSkills } from './skills';
+import { items } from './skills';
 
 export const title = 'Home';
 
-export const name = 'Name';
+export const name = 'Riadh';
 
-export const lastName = 'LastName';
+export const lastName = 'Adrani';
 
-export const description =
-	'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, rerum. Debitis accusantium deleniti enim iste dignissimos? Similique, exercitationem! Odit vero, numquam quae ratione maxime sunt reiciendis laudantium quaerat iure ipsum!';
+export const description = 'A self-made Software Engineer and a hobbyist Designer';
 
 export const links: Array<{ platform: Platform; link: string }> = [
-	{ platform: Platform.GitHub, link: 'https://github.com/' },
 	{
-		platform: Platform.Linkedin,
-		link: 'https://www.linkedin.com/'
+		platform: Platform.GitHub,
+		link: 'https://github.com/RiadhAdrani'
 	},
 	{
-		platform: Platform.Twitter,
-		link: 'https://twitter.com/'
+		platform: Platform.Linkedin,
+		link: 'https://www.linkedin.com/in/riadh-adrani/'
 	},
 	{
 		platform: Platform.StackOverflow,
-		link: 'https://stackoverflow.com/'
+		link: 'https://stackoverflow.com/users/12978534/riadh-adrani'
 	},
 	{
 		platform: Platform.Email,
 		link: 'riadh_adrani@hotmail.fr'
-	},
-	{
-		platform: Platform.Youtube,
-		link: 'https://www.youtube.com'
-	},
-	{
-		platform: Platform.Facebook,
-		link: 'https://www.facebook.com'
 	}
 ];
 
-export const skills = getSkills('js', 'css', 'html', 'reactjs', 'sass', 'svelte', 'ts');
+export const skills = items;
