158a66bdcba1377c8dd864216906cf71a97a777a
RiadhAdrani <riadh_adrani@hotmail.fr>
2024-03-02 22:42:06 +0100

    feat: split skills into categories

--- src/lib/data/home.ts
+++ src/lib/data/home.ts
@@ -1,16 +1,16 @@
-import { Platform, type HomeLink } from '$lib/types';
+import { Platform } from '$lib/types';
 import { getSkills } from './skills';
 
 export const title = 'Home';
 
 export const name = 'Name';
 
-export const lastName = 'Name';
+export const lastName = 'LastName';
 
 export const description =
 	'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, rerum. Debitis accusantium deleniti enim iste dignissimos? Similique, exercitationem! Odit vero, numquam quae ratione maxime sunt reiciendis laudantium quaerat iure ipsum!';
 
-export const links: Array<HomeLink> = [
+export const links: Array<{ platform: Platform; link: string }> = [
 	{ platform: Platform.GitHub, link: 'https://github.com/' },
 	{
 		platform: Platform.Linkedin,
