21a843bd45488d203a81176222d72ba76231b772
Frnasbukhari <64270666+Frnasbukhari@users.noreply.github.com>
2024-11-09 16:45:37 +0000

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const base = '/slick-portfolio-svelte';
+const base = '';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
